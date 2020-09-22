730fb3a94aec26156c53163f6768e2785a37ebfd
JokerV <Quocvietcoi2000@gmail.com>
2020-09-22 16:59:24 +0700

    Web

---
+++ precache-manifest.3c351861e11f31670400291f219a180b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25df6a7127de0b8fd96c38f4d595cd91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3657c3ec5afc4c6fd19f",
+    "url": "/static/css/2.366b4564.chunk.css"
+  },
+  {
+    "revision": "584bf389fd59a3e1530d",
+    "url": "/static/css/main.dc94e702.chunk.css"
+  },
+  {
+    "revision": "3657c3ec5afc4c6fd19f",
+    "url": "/static/js/2.c924e476.chunk.js"
+  },
+  {
+    "revision": "7f877ba9ec04f2b537b0dbeae9ef2789",
+    "url": "/static/js/2.c924e476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584bf389fd59a3e1530d",
+    "url": "/static/js/main.af06b2c6.chunk.js"
+  },
+  {
+    "revision": "724053adb4d58b59820a",
+    "url": "/static/js/runtime-main.fd6351d2.js"
+  },
+  {
+    "revision": "0db869e859eb2670a38cbd5bf33986c5",
+    "url": "/static/media/BeautyHealth.0db869e8.svg"
+  },
+  {
+    "revision": "2449a58367fa38603305c8e01fe069da",
+    "url": "/static/media/Beverage.2449a583.svg"
+  },
+  {
+    "revision": "3bb373088806e94dc29e63923453b272",
+    "url": "/static/media/Breakfast.3bb37308.svg"
+  },
+  {
+    "revision": "8322550daa3817dee04f8b3ec3adabc5",
+    "url": "/static/media/Cart.8322550d.svg"
+  },
+  {
+    "revision": "33d7e5d85765a1e61dd0efdcc8c77e10",
+    "url": "/static/media/Checkout.33d7e5d8.svg"
+  },
+  {
+    "revision": "e104eaace3bf113aa7feba3dffeff900",
+    "url": "/static/media/Cooking.e104eaac.svg"
+  },
+  {
+    "revision": "c7fd5c4aa3a3f4b0c26fb0084e36896f",
+    "url": "/static/media/Dairy.c7fd5c4a.svg"
+  },
+  {
+    "revision": "1bb007e494a04da38a8331c98904cc7a",
+    "url": "/static/media/English.1bb007e4.svg"
+  },
+  {
+    "revision": "a9e4ce7277f2fb599a3d620f635abd01",
+    "url": "/static/media/FruitsVegetable.a9e4ce72.svg"
+  },
+  {
+    "revision": "26f8a85720cb3d5dbfdafa3007eea79e",
+    "url": "/static/media/German.26f8a857.svg"
+  },
+  {
+    "revision": "b8f22f7a58724d3d3defdd4e811991aa",
+    "url": "/static/media/HomeCleaning.b8f22f7a.svg"
+  },
+  {
+    "revision": "88d7f50ac2c929b6e32934dcb2baa646",
+    "url": "/static/media/MeatFish.88d7f50a.svg"
+  },
+  {
+    "revision": "464d343b4e32633aa09092ee4413be82",
+    "url": "/static/media/PetCare.464d343b.svg"
+  },
+  {
+    "revision": "ecd8d15ee69c3b35700295c5ed19c56b",
+    "url": "/static/media/Snacks.ecd8d15e.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
